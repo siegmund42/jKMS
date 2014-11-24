927b48362bcc4f512cb9b5ea9fc5059e42ad34d7
siegmund42 <l.jaehn@gmx.de>
2014-11-24 11:04:33 +0100

    Tasks aufgeteilt

--- src/main/java/jKMS/states/State.java
+++ src/main/java/jKMS/states/State.java
@@ -1,24 +1,27 @@
 package jKMS.states;
 
+import com.itextpdf.text.Document;
+
 import jKMS.Contract;
 import jKMS.Kartoffelmarktspiel;
 
 public abstract class State {
 	protected Kartoffelmarktspiel kms;
 	
-	public void loadStandardDistribution(){}
-	public void generateCards(){}
-	public void newGroup(boolean isBuyer, int price, int relativeNumber){}
-	public void setBasicConfig(int playerCount, int assistantCount){}
-	public String createPDF(){ return ""; }
+	public void loadStandardDistribution(){} // LUKAS
+	public void generateCards(){} // JUSTUS
+	public void newGroup(boolean isBuyer, int price, int relativeNumber){} // 
+	public void setBasicConfig(int playerCount, int assistantCount){} // DOMINIK
+	public String createPDF(Document document, boolean isBuyer){ return ""; } // 
 	public boolean removeCard(){ return false; } //Memo: Update Verteilungsset
-	public boolean addContract(){ return false; }
-	public float equilibriumPrice(){ return 0; }
-	public float equilibriumSet(){ return 0; }
-	public float averagePrice(){ return 0; }
-	public Contract pickWinner(){ return null; }
-	public int buyerProfit(){ return 0; }
-	public int sellerProfit(){ return 0; }
+	public boolean addContract(int id1, int id2, int price){ return false; } // XINYU
+																			// TIMON implementiert Contract Klasse
+	public float equilibriumPrice(){ return 0; } // TIMON
+	public float equilibriumSet(){ return 0; } // TIMON
+	public float averagePrice(){ return 0; } // TIMON
+	public Contract pickWinner(){ return null; } // TIMON
+	public int buyerProfit(){ return 0; } // TIMON
+	public int sellerProfit(){ return 0; } // TIMON
 	public boolean save(String path){ return false; }
 	public void load(){}
 }
